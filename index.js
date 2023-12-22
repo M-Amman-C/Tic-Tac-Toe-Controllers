bd5e5a063e0fd2caa26ce549b07e48d98a135b69
Amman Chopadiya <ammanchopadiya15@gmail.com>
2023-12-22 10:28:00 +0530

    removing vercel build

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ import {fileURLToPath} from 'url';
 const __filename = fileURLToPath(import.meta.url);
 console.log(__filename)
 const __dirname = path.dirname(__filename);
-console.log(__dirname)
 
 const app = express();
-const port = 9001;
+const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("views", __dirname + "/views");
@@ -38,9 +37,9 @@ app.post("/",(req,res)=>{
 })
 
 app.get("/game",(req,res)=>{
-    /*if (player1=="" || player2==""){
+    if (player1=="" || player2==""){
         res.redirect("/home");
-    }*/
+    }
     res.render("game.ejs",{player1: player1,player2: player2,player1Score: player1Score,player2Score: player2Score});
 })
 
@@ -56,5 +55,5 @@ app.post("/game",(req,res)=>{
 })
 
 app.listen(port,()=>{
-    console.log("Server running on port "+port+"...");
+    console.log("Server running on port "+port+"...\n link: http://localhost:3000/");
 });
